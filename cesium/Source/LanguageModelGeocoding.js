18a4077ed7880d361fb311bcffee878975997a4c
yuvalsolaz <yuval.solaz@gmail.com>
2022-07-17 13:55:43 +0300

    Language model geocoding service

---
+++ cesium/Source/LanguageModelGeocoding.js
@@ -0,0 +1,44 @@
+/**
+ * This class provides geocoding through the Language model geocoding service.
+ * @alias LanguageModelGeocoder
+ * @constructor
+ */
+function LanguageModelGeocoder() {}
+
+/**
+ * The function called to geocode using this geocoder service.
+ *
+ * @param {String} input The query to be sent to the geocoder service
+ * @returns {Promise<GeocoderService.Result[]>}
+ */
+LanguageModelGeocoder.prototype.geocode = function (input) {
+  const endpoint = "https://nominatim.openstreetmap.org/search";        // TODO: replace with LM geocoding url when ready 
+  const resource = new Cesium.Resource({
+    url: endpoint,
+    queryParameters: {
+      format: "json",
+      q: input,
+    },
+  });
+
+  return resource.fetchJson().then(function (results) {
+    let bboxDegrees;
+    return results.map(function (resultObject) {
+      bboxDegrees = resultObject.boundingbox;
+      return {
+        displayName: resultObject.display_name,
+        destination: Cesium.Rectangle.fromDegrees(
+          bboxDegrees[2],
+          bboxDegrees[0],
+          bboxDegrees[3],
+          bboxDegrees[1]
+        ),
+      };
+    });
+  });
+};
+
+const viewer = new Cesium.Viewer("cesiumContainer", {
+  geocoder: new LanguageModelGeocoder(),
+});
+
